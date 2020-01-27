d3e7beb6fc1c4e15eef948ef9881f1ffd9a12662
Ariestellar <ahdrei47@gmail.com>
2020-01-27 15:20:23 +0300

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField] private Transform _targetEarth;
+    private Vector3 _offset;
+    private float _sensitivity = 3;
+    private float _zoomSpeed = 10;
+    private float _offsetX, _offsetY;
+
+    private void Start()
+    {
+        _offset.z = -400;
+    }
+
+    private void Update()
+    {
+        ZoomCamera();
+        RotateCamera();
+    }
+
+    private void ZoomCamera()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            _offset.z += _zoomSpeed;
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            _offset.z -= _zoomSpeed;
+    }
+
+    private void RotateCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _offsetX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * _sensitivity;
+            _offsetY += Input.GetAxis("Mouse Y") * _sensitivity;
+            transform.localEulerAngles = new Vector3(-_offsetY, _offsetX, 0);
+        }
+        _offset.z = Mathf.Clamp(_offset.z, -400, -200);
+        transform.position = transform.localRotation * _offset + _targetEarth.position;
+    }
+}
